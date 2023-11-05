8a08128cc837e76af6c4aaf842a3e4b028fb6134
Saeid Alizadeh <saeidalz96@gmail.com>
2023-11-04 21:48:06 -0600

    Further development; added expenses and balance

--- lifeStyleFront/src/finance/expense/SubmitExpenses.tsx
+++ lifeStyleFront/src/finance/expense/SubmitExpenses.tsx
@@ -1,21 +1,66 @@
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { useParams, NavLink } from "react-router-dom";
 import rl from "../../svg/RotatingLoad.svg";
 import BACKEND_URL from "../../Config";
 import Urls from "../../Urls";
 import StatusCodes from "../../StatusCodes";
 
+interface IBalance {
+  balanceId: number;
+  budgetId: number;
+  userId: number;
+  capital: number;
+  eatout: number;
+  entertainment: number;
+  total: number;
+  createdAt: Date;
+}
+
 const SubmitExpenses = () => {
   const { id } = useParams();
+  const mounted = useRef(true);
+  const [balance, setBalance] = useState<IBalance | null >(null);
+
   const minMoney = "1.00";
   const expenseTypeRef = useRef<HTMLSelectElement>(null);
   const expenseAmountRef = useRef<HTMLInputElement>(null);
   const expenseDescRef = useRef<HTMLInputElement>(null);
-  
+
   const [loading, setLoading] = useState<boolean>(false);
   const [possibleErrs, setPossibleErrs] = useState(false);
   const [possibleErrsMsg, setPossibleErrsMsg] = useState("");
   const [successRes, setSuccessRes] = useState(false);
+
+  useEffect(() => {
+    if (mounted.current) {
+      mounted.current = false;
+
+      const fetchSingleBalance = async () => {
+        const result = await fetch(
+          `${BACKEND_URL}${Urls.finance.index}/${Urls.finance.balance}/${id}`,
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
+
+        if (result.status === StatusCodes.Accepted) {
+          const data = await result.json();
+          setBalance(data);
+        } else if (result.status === StatusCodes.NoContent) {
+          setBalance(null);
+        } else if (result.status === StatusCodes.InternalServerError) {
+          console.log("Failed to fetch the data!");
+          setBalance(null);
+        } else {
+          console.log("Unexpected error happened!");
+          setBalance(null);
+        }
+      };
+
+      fetchSingleBalance();
+    }
+  });
 
   async function handleSubmitExpense(e: FormEvent) {
     e.preventDefault();
@@ -125,9 +170,9 @@ const SubmitExpenses = () => {
                   id="expenseType"
                   ref={expenseTypeRef}
                 >
-                  <option value="capital">Capital</option>
-                  <option value="eatout">Eat Out</option>
-                  <option value="entertainment">Entertainment</option>
+                  <option value="capital">Capital &#x1F449; Left: ${balance ? balance.capital: "" }</option>
+                  <option value="eatout">Eat Out &#x1F449; Left: ${balance ? balance.eatout: "" }</option>
+                  <option value="entertainment">Entertainment &#x1F449; Left: ${balance ? balance.entertainment: "" }</option>
                 </select>
               </div>
               <div className="mx-5">
