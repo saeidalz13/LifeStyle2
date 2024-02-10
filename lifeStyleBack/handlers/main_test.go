4af2ebc73fb478c80cdbdd1693135d053f26322b
Saeid Alizadeh <saeidalz96@gmail.com>
2024-02-10 10:40:42 -0700

    Fixed issues with global vars in test handlers

--- lifeStyleBack/handlers/main_test.go
+++ lifeStyleBack/handlers/main_test.go
@@ -11,13 +11,12 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	cn "github.com/saeidalz13/LifeStyle2/lifeStyleBack/config"
 	sqlc "github.com/saeidalz13/LifeStyle2/lifeStyleBack/db/sqlc"
 	"github.com/saeidalz13/LifeStyle2/lifeStyleBack/token"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
+	// "golang.org/x/oauth2"
+	// "golang.org/x/oauth2/google"
 )
 
 const TEST_DB_DRIVER = "postgres"
@@ -82,51 +81,33 @@ func ensureFitnessMovesAvailability() error {
 	return nil
 }
 
-func getEnvVars() (*cn.DotEnvVars, error) {
-	if err := godotenv.Load("../.env"); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-		return nil, err
-	}
-
-	return &cn.DotEnvVars{
-		FrontEndUrl: os.Getenv("FRONTENDURL"),
-		Port:        os.Getenv("PORT"),
-		PasetoKey:   os.Getenv("PASETO_KEY"),
-		DbUrl:       os.Getenv("DATABASE_URL"),
-		DbTestUrl:   os.Getenv("DB_TEST_URL"),
-		DevStage:    os.Getenv("DEV_STAGE"),
-		GClientId:   os.Getenv("GOOGLE_CLIENT_ID"),
-		GClientSec:  os.Getenv("GOOGLE_CLIENT_SEC"),
-		GRedirUrl:   os.Getenv("GOOGLE_REDIRECT_URL"),
-		GptApiKey:   os.Getenv("GPT_API_KEY"),
-	}, nil
-}
 
 // Entry point of all the tests in handlers package
 func TestMain(m *testing.M) {
-	envVarsTest, err := getEnvVars()
-	if err != nil {
-		log.Println("Failed to retrieve data from dotenv file")
-		panic(err.Error())
-	}
-	googleOAuthConfig := &oauth2.Config{
-		RedirectURL:  envVarsTest.GRedirUrl,
-		ClientID:     envVarsTest.GClientId,
-		ClientSecret: envVarsTest.GClientSec,
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-		Endpoint:     google.Endpoint,
+	// googleOAuthConfig := &oauth2.Config{
+	// 	RedirectURL:  envVarsTest.GRedirUrl,
+	// 	ClientID:     envVarsTest.GClientId,
+	// 	ClientSecret: envVarsTest.GClientSec,
+	// 	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+	// 	Endpoint:     google.Endpoint,
+	// }
+
+	randomString32Chars := "c808cd4bc8639e0808216f5180277189"
+
+	cn.EnvVars = &cn.DotEnvVars{
+		DbTestUrl: "postgresql://root:testpassword@localhost:5432/lfdb?sslmode=disable",
+		PasetoKey: randomString32Chars,
+		DevStage:  "dev",
 	}
 
-	tempPaseto, err := token.NewPasetoMaker(envVarsTest.PasetoKey)
+	// cn.OAuthConfigFitFin = googleOAuthConfig
+	tempPaseto, err := token.NewPasetoMaker(cn.EnvVars.PasetoKey)
 	if err != nil {
 		log.Fatalln("Failed to extract Paseto Key!")
 	}
-
-	cn.EnvVars = envVarsTest
-	cn.OAuthConfigFitFin = googleOAuthConfig
 	token.PasetoMakerGlobal = tempPaseto
 
-	DB_TEST, err := sql.Open(TEST_DB_DRIVER, envVarsTest.DbTestUrl)
+	DB_TEST, err := sql.Open(TEST_DB_DRIVER, cn.EnvVars.DbTestUrl)
 	if err != nil {
 		log.Println(err)
 	}
